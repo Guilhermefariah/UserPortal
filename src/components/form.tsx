7d5ef5b6f874d9b581fe5a5bb658f204fd967a90
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-07 15:00:49 -0300

    Vincula o estado 'name'. Quando valor do estado muda, 'setName' é chamado para atualizar o estado

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 
 const Form: React.FC = () => {
     const [name, setName] = useState('');
-    
+
     return (
         <form>
             <div>
-
+                <label>
+                    Name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="w-full border rounded"
+                />
             </div>
             <button>
-                
+
             </button>
         </form>
     )
