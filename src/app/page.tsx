3b68917e640c631ce55efc7946b131e01b6c26bb
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-07 15:49:36 -0300

    incluindo formulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
+import Form from "@/components/form"
 
 const Page: React.FC = () => {
   return (
     <div>
       <main className="flex items-center justify-center min-h-screen bg-gray-100" >
-
+        <Form />
       </main>
     </div>
   )
