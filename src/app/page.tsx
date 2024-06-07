5cdcb95606fd6359cd0944bcafd50280933e77b2
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-07 17:21:55 -0300

    defindo conjunto de teste para o componente Form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Form from "@/components/form"
+import Form from "@/components/Form"
 
 const Page: React.FC = () => {
   return (
--- src/components/form.test.tsx
+++ src/components/form.test.tsx
@@ -1,3 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import Form from "./form";
+import Form from "./Form";
 
+describe('Form', () => {
+    
+})
