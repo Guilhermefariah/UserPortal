65f33fefef0d58fd2bde228874221f8d68657407
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-07 14:17:44 -0300

    exportando o componente Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ const Page: React.FC = () => {
   return (
     <div>
       <main className="flex items-center justify-center min-h-screen bg-gray-100" >
-          
+
       </main>
     </div>
   )
 }
+export default Page
