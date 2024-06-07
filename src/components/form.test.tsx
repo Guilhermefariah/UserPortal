6825bf192edd6c26c70a7f1f3b9a6911c623a4d4
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-07 17:24:07 -0300

    teste para verificar se o button de submit e renderizado

--- src/components/form.test.tsx
+++ src/components/form.test.tsx
@@ -2,5 +2,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Form from "./Form";
 
 describe('Form', () => {
-    
+    it('renders the submit button', () => {
+        
+    })
 })
